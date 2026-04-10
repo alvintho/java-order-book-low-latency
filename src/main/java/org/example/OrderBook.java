a40a5cb8d069b9007eed471a94ae9304b95f9689
alvintho <alvinthosatria@gmail.com>
2026-04-11 00:54:04 +0800

    feat: implement add order to order book with TDD

--- src/main/java/org/example/OrderBook.java
+++ src/main/java/org/example/OrderBook.java
@@ -1,11 +1,34 @@
 package org.example;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class OrderBook {
+    private double totalVolume; // sum of all Bids + Sum of all Asks
+    private final TreeMap<Double, Queue<Order>> bids = new TreeMap<>(Collections.reverseOrder());
+    private final TreeMap<Double, Queue<Order>> asks = new TreeMap<>();
+    private final Map<UUID, Order> orderMap = new HashMap<>();
 
-    public float getTotalVolume() {
-        return 0;
+    public OrderBook() {
+        this.totalVolume = 0;
+    }
+
+    public double getTotalVolume() {
+        return this.totalVolume;
+    }
+
+    public void addOrder(Order order) {
+        TreeMap<Double, Queue<Order>> book = order.isBid() ? bids : asks;
+
+        book.computeIfAbsent(order.getPrice(), k -> new LinkedList<>()).add(order);
+
+        this.orderMap.put(order.getOrderId(), order);
+    }
+
+    public double getBestBid() {
+        return bids.isEmpty() ? Double.NaN : bids.firstKey();
+    }
+
+    public double getBestAsk() {
+        return asks.isEmpty() ? Double.NaN : asks.firstKey();
     }
 }
--- src/test/java/org/example/OrderBookTest.java
+++ src/test/java/org/example/OrderBookTest.java
@@ -1,15 +1,38 @@
 package org.example;
 
-import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.UUID;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 
 class OrderBookTest {
+    private OrderBook orderBook;
+
+    @BeforeEach
+    void setUp() {
+        orderBook = new OrderBook();
+    }
 
     @Test
-    public void testNewOrderBookShouldBeEmpty() {
-        OrderBook orderBook = new OrderBook();
-        assertEquals(0, orderBook.getTotalVolume());
+    void shouldAddBuyOrderToBids() {
+        Order order = new Order(UUID.randomUUID(), 100.0, 10.0, Side.BUY);
+
+        orderBook.addOrder(order);
+
+        assertEquals(100.0, orderBook.getBestBid());
+        assertEquals(Double.NaN, orderBook.getBestAsk());
+    }
+
+    @Test
+    void shouldAddSellOrderToAsks() {
+        Order order = new Order(UUID.randomUUID(), 100.0, 10.0, Side.SELL);
+
+        orderBook.addOrder(order);
+
+        assertEquals(100.0, orderBook.getBestAsk());
+        assertEquals(Double.NaN, orderBook.getBestBid());
     }
 }
